df8cd1d6b173a23cfeacef41837de2cfdef2e11b
Sparx92 <stefankoserkov@gmail.com>
2016-12-12 21:20:40 +0200

    Enabled CORS

--- server/config/express.js
+++ server/config/express.js
@@ -14,5 +14,11 @@ module.exports = () => {
     }));
     passportConfig(app);
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+    
     return app;
 };
