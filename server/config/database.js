570179633a5a4d9ecac867ed926f9143d27a1e7d
belchevgb <belchev.gb@gmail.com>
2016-12-13 15:00:57 +0300

    Mongoose sucks

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 module.exports = config => {
-    mongoose.connect(config.connectionString)
-        .then(console.log)
-        .catch(console.log);
+    mongoose.connect(config.connectionString).catch(console.log);
+
+    let db = mongoose.connection;
+    db.on('error', (err) => console.log(err));
 };
