04338ab690cb845ee5a6e7ee0547d727b21f802e
Sparx92 <stefankoserkov@gmail.com>
2016-12-18 14:56:50 +0200

    :cat:

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("Dummy", () => {
+describe("Dummy1", () => {
     it("Hi Travis", () => {
         expect(0).to.equal(0);
     });
