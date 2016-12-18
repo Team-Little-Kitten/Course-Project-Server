994c0ea4971acc7b024e1105fab255e55bde63f6
belchevgb <belchev.gb@gmail.com>
2016-12-18 13:13:01 +0200

    Facebook login route is added, but is not completed.

--- server/config/express.js
+++ server/config/express.js
@@ -7,7 +7,14 @@ const cors = require("cors");
 module.exports = () => {
     let app = express();
 
-    app.options('*', cors());
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
+    app.options("*", cors());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(expressSession({
@@ -16,12 +23,5 @@ module.exports = () => {
         saveUninitialized: true
     }));
     passportConfig(app);
-
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
-
     return app;
 };
--- server/routers/auth-router.js
+++ server/routers/auth-router.js
@@ -1,8 +1,20 @@
 "use strict";
 
+const passport = require("passport");
+
+function facebookCallback(req, res) {
+    res.json({
+        success: true,
+        message: "Login successful."
+    });
+}
+
 module.exports = ({ app, controllers }) => {
     app.post("/auth/register", controllers.auth.registerUser);
     app.post("/auth/login", controllers.auth.loginUser);
     app.post("/auth/logout", controllers.auth.logoutUser);
     app.post("/auth/verify", controllers.auth.verifyLogin);
+
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), facebookCallback);
 };
