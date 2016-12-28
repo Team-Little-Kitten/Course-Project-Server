0abc8f9774cb3f709de74f66f1562c229de0ae70
Sparx92 <stefankoserkov@gmail.com>
2016-12-28 19:12:04 +0200

    improvements to notifictions

--- server/controllers/notifications-controller.js
+++ server/controllers/notifications-controller.js
@@ -1,5 +1,15 @@
 const User = require("./../models/user");
 
+function getElementIndex(array, attr, value) {
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i][attr].toString() === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
 module.exports = () => {
     return {
         refreshNotification(req, res) {
@@ -8,6 +18,23 @@ module.exports = () => {
             User.findOne({ username })
                 .then(foundUser => {
                     return res.json(foundUser.notifications);
+                });
+        },
+        markNotificationAsRead(req, res) {
+            let notificationId = req.query.id;
+            let username = req.query.username;
+            console.log(notificationId, username);
+            User.findOne({ username })
+                .then(user => {
+                    let notifications = user.notifications;
+                    let index = getElementIndex(notifications, "_id", notificationId);
+                    let notificationToPush = notifications[index];
+                    notificationToPush.isRead = true;
+                    notificationToPush.readOn = Date.now();
+                    user.notifications.splice(index, 1, notificationToPush);
+
+
+                    user.save();
                 });
         }
     };
--- server/routers/notifications-router.js
+++ server/routers/notifications-router.js
@@ -1,3 +1,4 @@
 module.exports = ({ app, controllers }) => {
     app.get("/api/notifications/refresh", controllers.notifications.refreshNotification);
+    app.get("/api/notifications/markAsRead", controllers.notifications.markNotificationAsRead);
 };
